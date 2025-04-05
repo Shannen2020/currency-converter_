0b57de7ca8bad95919f1f4d88b2353638ce1ced8
shannen2020 <shannen.lam@gmail.com>
2025-04-05 12:01:10 +0800

    add ex4,5 unit tests

--- src/components/CurrencyConverter.test.jsx
+++ src/components/CurrencyConverter.test.jsx
@@ -2,6 +2,7 @@ import { screen, render } from '@testing-library/react';
 import { describe, it, expect } from 'vitest'
 import CurrencyConverter from './CurrencyConverter'
 import CurrencyContext from '../contexts/CurrencyContext';
+import { fireEvent, rerender } from '@testing-library/react';
 
 describe('CurrencyConverter Component', () => {
     it('should render correctly', () => {
@@ -29,4 +30,120 @@ describe('CurrencyConverter Component', () => {
         // Assert
 
     })
+
+    it("should show the correct output for a given input", () => {
+        // Arrange
+        render (
+            <CurrencyContext.Provider value={{
+                fromCurrency: "SGD",
+                toCurrency: "USD"
+            }}>
+                <CurrencyConverter />
+                </CurrencyContext.Provider>
+                )
+
+        // Act
+        const fromAmountInput = screen.getByPlaceholderText("Enter amount")
+        fireEvent.change(fromAmountInput, { target: { value:10 } } )
+
+        // Assert
+        expect(screen.getByText("20")).toBeInTheDocument()
+    })
 })
+
+    // Exercise 3
+    it("should ...", () =>{
+        // Arrange
+        render (
+            <CurrencyContext.Provider value={{
+                fromCurrency: "SGD",
+                toCurrency: "USD"
+            }}>
+                <CurrencyConverter />
+                </CurrencyContext.Provider>
+                )
+        //Act 
+        const Blankamount = screen.getByPlaceholderText("Enter amount")
+        fireEvent.change(Blankamount, { target: { value:"" } } )
+
+        // Assert
+        expect(screen.getByText("0")).toBeInTheDocument()
+    })
+
+    // exercise 4
+
+    it("should show updated currencies afer user selects new currency", () => {
+        //Arange
+        const { rerender } = render (
+            <CurrencyContext.Provider value={{
+                fromCurrency: "SGD",
+                toCurrency: "USD"
+            }}>
+                <CurrencyConverter />
+                </CurrencyContext.Provider>
+                )
+        
+        //Act
+
+        //Assert
+        expect(screen.getByText("SGD")).toBeInTheDocument()
+        expect(screen.getByText("USD")).toBeInTheDocument()
+
+
+        // user has selected new currencies, context is updated
+
+        // Arrange
+        rerender(
+            <CurrencyContext.Provider value={{
+                fromCurrency: "GBP",
+                toCurrency: "JPY"
+            }}>
+                <CurrencyConverter />
+                </CurrencyContext.Provider>
+                )
+
+        //Assert
+        expect(screen.getByText("GBP")).toBeInTheDocument()
+        expect(screen.getByText("JPY")).toBeInTheDocument()
+    })
+
+    // exercise 5
+
+    it("should show updated currencies afer user selects new currency + added amount", () => {
+        //Arange
+        const { rerender } = render (
+            <CurrencyContext.Provider value={{
+                fromCurrency: "SGD",
+                toCurrency: "USD"
+            }}>
+                <CurrencyConverter />
+                </CurrencyContext.Provider>
+                )
+        
+        //Act
+        const AmountUpdated = screen.getByPlaceholderText("Enter amount")
+        fireEvent.change(AmountUpdated, { target: { value:"10" } } )
+
+        //Assert
+        expect(screen.getByText("SGD")).toBeInTheDocument()
+        expect(screen.getByText("USD")).toBeInTheDocument()
+        expect(screen.getByText("20")).toBeInTheDocument()
+        
+        // user has selected new currencies, context is updated
+
+        // Arrange
+        rerender(
+            <CurrencyContext.Provider value={{
+                fromCurrency: "GBP",
+                toCurrency: "JPY"
+            }}>
+                <CurrencyConverter />
+                </CurrencyContext.Provider>
+                )
+
+        //Assert
+        expect(screen.getByText("GBP")).toBeInTheDocument()
+        expect(screen.getByText("JPY")).toBeInTheDocument()
+        expect(screen.getByText("20")).toBeInTheDocument() // weird
+    })
+ 
